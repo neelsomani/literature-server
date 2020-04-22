a0590eb5a7868ded74328304c1e75bf2e0205c76
Neel Somani <neel@berkeley.edu>
2020-04-22 03:40:44 -0700

    Define remainder of API

--- backend.py
+++ backend.py
@@ -6,7 +6,16 @@ from constants import *
 
 
 class LiteratureAPI:
-    """ Interface for registering and updating WebSocket clients. """
+    """
+    Interface for registering and updating WebSocket clients
+    for a given game.
+
+    TODO(@neel): Add authentication keys for users.
+    Send authentication key when the client registers.
+    TODO(@neel): Add move_timestamp + game parameters such as time limit.
+    # Add n_players and start the game when enough players are connected.
+    TODO(@neel): Implement API.
+    """
 
     def __init__(self, logger):
         self.clients = list()
@@ -36,7 +45,6 @@ class LiteratureAPI:
     def handle_message(self, message):
         action_map = {
             CLAIM: self._claim,
-            HAND: self._hand,
             MOVE: self._move,
             SWITCH_TEAM: self._switch_team
         }
@@ -50,13 +58,56 @@ class LiteratureAPI:
         fn(message.get('payload', {}))
 
     def _claim(self, payload):
-        pass
-
-    def _hand(self, payload):
-        pass
+        """
+        Evaluate whether a player's claim is valid.
+        Send the whether the player was correct in addition
+        to the correct matchings to all players.
+        """
+        raise NotImplementedError
 
     def _move(self, payload):
-        pass
+        """
+        Execute a move for a player.
+        """
+        raise NotImplementedError
 
     def _switch_team(self, payload):
-        pass
+        """
+        Check if the current player's time is up. If so,
+        switch the team and send the players the updated
+        game state.
+        """
+        raise NotImplementedError
+
+    def _send_updated_game_state(self):
+        """
+        Send all necessary info to players for the beginning
+        of the turn.
+        """
+        self._send_last_move()
+        self._send_hands()
+
+    def _send_last_move(self):
+        """
+        Send the last move and each player's number of cards to
+        all players.
+        """
+        raise NotImplementedError
+
+    def _send_hands(self):
+        """
+        Send each player their hand.
+        """
+        raise NotImplementedError
+
+    def _send_complete(self):
+        """
+        Send that the game is in a finished state to all players.
+        """
+        raise NotImplementedError
+
+    def _send_score(self):
+        """
+        Send the updated score to all players.
+        """
+        raise NotImplementedError
