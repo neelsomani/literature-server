f9fe3671801edb65601288509813bb38618e4f86
Neel Somani <neel@berkeley.edu>
2020-04-22 02:07:34 -0700

    Add WebSockets to the server (#1)

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
+import json
 import os
+
 from flask import Flask
+from flask_sockets import Sockets
+import gevent
+
+from backend import LiteratureAPI
 
 app = Flask(__name__, static_folder='build/', static_url_path='/')
 app.debug = 'DEBUG' in os.environ
+
+sockets = Sockets(app)
+api = LiteratureAPI()
 
 
 @app.route('/')
@@ -13,3 +22,31 @@ def index():
 @app.route('/<path:path>')
 def static_file(path):
     return app.send_static_file(path)
+
+
+@sockets.route('/submit')
+def submit(ws):
+    """ Receives incoming chat messages, sends to all clients. """
+    while not ws.closed:
+        gevent.sleep(0.1)
+        msg = ws.receive()
+        if not msg:
+            continue
+
+        try:
+            user_msg = json.loads(msg)
+        except json.decoder.JSONDecodeError as _:
+            app.logger.exception('Exception in parsing JSON message: {}'
+                                 .format(msg))
+            continue
+
+        app.logger.info('Handling message: {}'.format(msg))
+        api.handle_message(user_msg)
+
+
+@sockets.route('/receive')
+def receive(ws):
+    """ Register the WebSocket. """
+    api.register(ws)
+    while not ws.closed:
+        gevent.sleep(0.1)
--- src/App.js
+++ src/App.js
@@ -3,6 +3,40 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  handleMessage(message) {
+    let data = JSON.parse(message.data);
+    console.log('Received: ' + JSON.stringify(data));
+  }
+
+  sendMessage(message) {
+    this.sender.send(JSON.stringify({ message }));
+  }
+
+  componentDidMount() {
+    let ws_scheme;
+    if (window.location.protocol === "https:") {
+      ws_scheme = "wss://";
+    } else {
+      ws_scheme = "ws://"
+    };
+    let receiver = new window.ReconnectingWebSocket(
+      ws_scheme + location.host + "/receive"
+    );
+    receiver.onmessage = this.handleMessage;
+    receiver.onclose = function() {
+      this.receiver = new WebSocket(receiver.url);
+    };
+    let sender = new window.ReconnectingWebSocket(
+      ws_scheme + location.host + "/submit"
+    );
+    sender.onclose = function() {
+      this.sender = new WebSocket(sender.url);
+    };
+    this.setState({
+      'sender': sender
+    })
+  }
+
   render() {
     return (
       <div className="App">
