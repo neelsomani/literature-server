a89084550fa528ee52087c83c6f006cba4153954
Neel Somani <neel@berkeley.edu>
2020-04-24 03:40:29 -0700

    Increase test coverage

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,53 @@
 import React from 'react';
+import ReactTestUtils from 'react-dom/test-utils';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-class MockReconnectingWebSocket {
+class MockSocketWrapper {
+  socketClass() {
+    const parent = this;
+    class MockReconnectingWebSocket {
+      constructor(props) {
+        if (props.includes('receive')) {
+          parent.socket = this;
+        }
+      }
+    }
+    return MockReconnectingWebSocket;
+  }
 }
 
+function serialize(payload) {
+  return {
+    'data': JSON.stringify(payload)
+  };
+}
+
+let container;
+
+beforeEach(() => {
+  container = document.createElement('div');
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  document.body.removeChild(container);
+  container = null;
+});
+
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  window.ReconnectingWebSocket = MockReconnectingWebSocket;
+  const socketWrapper = new MockSocketWrapper();
+  window.ReconnectingWebSocket = socketWrapper.socketClass();
   window.cards = {}
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<App />, container);
+  socketWrapper.socket.onmessage(serialize(
+    {
+      'action': 'register',
+      'payload': {
+        'uuid': '0',
+        'player_n': 0,
+        'n_players': 4,
+        'time_limit': 30
+      }
+    }));
 });
